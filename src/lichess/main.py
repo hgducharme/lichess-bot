a310a7b1bd386d6f524ff253302f47d2ec90039e
Hunter Ducharme <hgducharme@gmail.com>
2022-04-12 17:21:23 -0500

    Added logging

--- src/lichess/main.py
+++ src/lichess/main.py
@@ -1,8 +1,21 @@
+import logging
 from conf import settings
 from LichessAPI import LichessAPI
 from LichessCLI import LichessCLI
 
 if __name__ == "__main__":
+    logger = logging.getLogger("lichess")
+    logger.setLevel(logging.DEBUG)
+    file_handler = logging.FileHandler("lichess.log")
+    file_handler.setLevel(logging.DEBUG)
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    file_handler.setFormatter(formatter)
+    console_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
+    logger.addHandler(console_handler)
+
     api = LichessAPI(settings.API_TOKEN)
     cli = LichessCLI(api)
     cli.run()
