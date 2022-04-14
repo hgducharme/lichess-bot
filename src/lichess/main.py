e21937e8860caafcd6ede718fdae934fa1d9814d
Hunter Ducharme <hgducharme@gmail.com>
2022-04-14 00:20:13 -0500

    Added filter and more file handlers

--- src/lichess/main.py
+++ src/lichess/main.py
@@ -3,7 +3,7 @@ from conf import settings
 from LichessAPI import LichessAPI
 from LichessCLI import LichessCLI
 
-class ThreadLogFilter(logging.Filter):
+class ThreadFilter(logging.Filter):
     """
     This filter only shows log entries for the specified thread name
     """
@@ -18,14 +18,27 @@ class ThreadLogFilter(logging.Filter):
 if __name__ == "__main__":
     logger = logging.getLogger("lichess")
     logger.setLevel(logging.DEBUG)
-    file_handler = logging.FileHandler("./logs/lichess.log")
-    file_handler.setLevel(logging.DEBUG)
+
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+    # Log handlers
+    challenge_handler_fh = logging.FileHandler("./src/lichess/logs/challenge_handler.log")
+    event_handler_fh = logging.FileHandler("./src/lichess/logs/event_handler.log")
+    lichess_fh = logging.FileHandler("./src/lichess/logs/lichess.log")
+    file_handlers = [challenge_handler_fh, event_handler_fh, lichess_fh]
+
+    # Add filters
+    thread_filter = ThreadFilter()
+    challenge_handler_fh.addFilter(ThreadFilter)
+    event_handler_fh.addFilter(ThreadFilter)
+    
+    for file_handler in file_handlers:
+        file_handler.setLevel(logging.DEBUG)
+        file_handler.setFormatter(formatter)
+        logger.addHandler(file_handler)
+
     console_handler = logging.StreamHandler()
     console_handler.setLevel(logging.ERROR)
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    file_handler.setFormatter(formatter)
-    console_handler.setFormatter(formatter)
-    logger.addHandler(file_handler)
     logger.addHandler(console_handler)
 
     api = LichessAPI(settings.API_TOKEN)
