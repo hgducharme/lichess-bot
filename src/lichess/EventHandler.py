26ab512655c2994997953fa43890d071a1a6885c
Hunter Ducharme <hgducharme@gmail.com>
2022-04-15 00:46:12 -0500

    Figured out how to parse event stream

--- src/lichess/EventHandler.py
+++ src/lichess/EventHandler.py
@@ -10,24 +10,45 @@ class EventHandler(Thread):
         Thread.__init__(self, **kwargs)
         self.api = lichess_api
         self.is_running = False
+        self.username = self.api.get_profile().json()["username"]
+        logger.debug(self.username)
 
     def run(self):
         logger.info("An EventHandler thread has been started")
-        if self.is_running == False:
-            self.is_running = True
+
+        self._toggle_running_flag()
 
         while self.is_running:
             event_stream = self.api.stream_events()
-            event_stream = self._parse_stream(event_stream)
-            logger.debug(f"Event stream output - {event_stream}")
 
-    def _parse_stream(self, stream):
-        items_in_stream = []
-        for line in stream:
-            if line:
-                items_in_stream.append(json.loads(line))
+            for line in event_stream:
+                if line:
+                    logger.debug(f"line = {json.loads(line)}")
+                    self._parse_line(line)
 
-        if len(items_in_stream) == 1:
-            return tuple(items_in_stream[0])
+    def _toggle_running_flag(self):
+        self.is_running = (self.is_running == False)
 
-        return tuple(items_in_stream)
+    def _parse_line(self, line):
+        category = line["type"]
+        if category == "challenge":
+            if self.is_external_challenge(line):
+                # TODO: Isn't this handled in the ChallengeHandler?
+                return 0
+            return 0
+        if category == "challengeDeclined":
+            return 0
+        if category == "gameStart":
+            return 0
+        if category == "gameFinish":
+            return 0
+        if category == "challengeCancelled":
+            return 0
+
+    def is_external_challenge(self, line):
+        is_external_challenge = True
+        challenger = line["challenge"]["challenger"]["name"]
+        if challenger == self.username:
+            is_external_challenge = False
+
+        return is_external_challenge
