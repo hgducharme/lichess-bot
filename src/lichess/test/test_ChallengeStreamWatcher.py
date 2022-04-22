02bdad7cf49dfad3c54b0fe2681222b52f12df10
Hunter Ducharme <hgducharme@gmail.com>
2022-04-22 00:04:44 -0500

    Added skeleton test code

---
+++ src/lichess/test/test_ChallengeStreamWatcher.py
@@ -0,0 +1,15 @@
+import pytest
+from GameManager import GameManager
+from LichessAPI import LichessAPI
+from ChallengeStreamWatcher import ChallengeStreamWatcher
+
+class TestChallengeStreamWatcher:
+    def __init__(self):
+        # Be careful with this. The init function is going to make calls to request.
+        # Figure out a way to mock this in the instantiation
+        # self.api = LichessAPI()
+        self.game_manager = GameManager()
+        self.challenge_stream_watcher = ChallengeStreamWatcher()
+
+    def test_maxGameLimit(self):
+        return False
