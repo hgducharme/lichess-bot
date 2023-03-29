5aa5d3d3b4c3baff6f5aca7c6112129dde21a2cb
Hunter Ducharme <hgducharme@gmail.com>
2023-03-29 15:23:19 -0700

    working on test file

--- src/lichess/test/test_ChallengeStreamWatcher.py
+++ src/lichess/test/test_ChallengeStreamWatcher.py
@@ -5,13 +5,24 @@ from src.lichess.LichessAPI import LichessAPI
 from src.lichess.ChallengeStreamWatcher import ChallengeStreamWatcher
 
 class TestChallengeStreamWatcher:
-    def setup_class(self):
-        # Be careful with this. The init function is going to make calls to request.
-        # Figure out a way to mock this in the instantiation
+
+    def setup_method(self):
+        # We need to mock requests.session.get and requests.session.post
+        
         # self.api = LichessAPI()
-        # self.game_manager = GameManager()
-        # self.challenge_stream_watcher = ChallengeStreamWatcher()
-        pass
+        self.game_manager = GameManager()
+        self.challenge_stream_watcher = ChallengeStreamWatcher()
+
+    def teardown_method(self):
+       pass
+
+    # def setup_class(self):
+    #     # Be careful with this. The init function is going to make calls to request.
+    #     # Figure out a way to mock this in the instantiation
+    #     # self.api = LichessAPI()
+    #     # self.game_manager = GameManager()
+    #     # self.challenge_stream_watcher = ChallengeStreamWatcher()
+    #     pass
 
     def test_maxGameLimit(self):
         assert False
