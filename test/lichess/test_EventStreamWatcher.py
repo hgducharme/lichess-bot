def06765bc9d8e691afda4c7682c3eb7021eb64a
Hunter Ducharme <hgducharme@gmail.com>
2023-05-12 15:57:06 -0700

    Nearly have a working test suite. Need to mock out ChessGame API calls

--- test/lichess/test_EventStreamWatcher.py
+++ test/lichess/test_EventStreamWatcher.py
@@ -4,8 +4,28 @@ from lichess.LichessAPI import LichessAPI
 from lichess.EventStreamWatcher import EventStreamWatcher
 
 @pytest.fixture(scope="module")
+def lichess_api():
+    api_session = requests.Session()
+    api_session.headers.update({"Authorization": f"Bearer {fake_oauth_token}"})
+    return LichessAPI(api_session)
+
+@pytest.fixture(scope="module")
+def game_manager(lichess_api):
+    return GameManager(lichess_api, engine_stub)
+
+@pytest.fixture(scope="function")
 @responses.activate
-def event_stream_watcher():
+def event_stream_watcher(lichess_api, game_manager, request):
+    # Get the marker
+    marker = request.node.get_closest_marker("set_fake_event")
+
+    # Get the parameter passed into the marker
+    fake_event = marker.args[0]
+
+    # This turns fake_event_stream into a byte-like iterable object which 
+    # is required to mock an API "stream" from the requests library
+    fake_event_stream = str.encode(fake_event)
+
     # EventManager queries the API for the lichess profile information upon instantiation,
     # so we tell responses to expect this call and mock it
     responses.add(
@@ -14,6 +34,7 @@ def event_stream_watcher():
         json = fake_profile_data,
         status = 200,
     )
+
     responses.add(
         responses.GET,
         LichessAPI.construct_url(LichessAPI.URL_ENDPOINTS["stream_events"]),
@@ -21,28 +42,35 @@ def event_stream_watcher():
         status = 200,
     )
 
-    # Setup dependencies
-    api_session = requests.Session()
-    api_session.headers.update({"Authorization": f"Bearer {fake_oauth_token}"})
-    api = LichessAPI(api_session)
-    game_manager = GameManager(api, engine_stub)
+    # TODO: This mock isn't working for some reason. The ChessGame request is hitting the actual 
+    # lichess servers
+    responses.add(
+        responses.GET,
+        LichessAPI.construct_url(LichessAPI.URL_ENDPOINTS["stream_bot_game_state"]),
+        body = fake_gameFull,
+        status = 200,
+    )
 
-    # Instantiate the class to test
-    return EventStreamWatcher(api, game_manager)
+    return EventStreamWatcher(lichess_api, game_manager)
 
 class TestEventStreamWatcher:
-    def test_challengeUserGetsAddedToChallengeQueue(self, event_stream_watcher):
-        test_user = "testuser"
+    @pytest.mark.set_fake_event(fake_gameStart)
+    def test_gameStartEventCreatesNewGameInGameManager(self, event_stream_watcher):
         event_stream_watcher.work()
-        event_stream_watcher.challenge_user(test_user)
-        assert test_user in self.event_stream_watcher.username_queue
 
-    def test_challengingEmptyUserDoesNothing(self, event_stream_watcher):
-        event_stream_watcher.challenge_user()
-        assert len(self.event_stream_watcher.username_queue) == 0
+    # def test_challengeUserGetsAddedToChallengeQueue(self, event_stream_watcher):
+    #     pass
+    #     test_user = "testuser"
+    #     event_stream_watcher.challenge_user(test_user)
+    #     assert test_user in self.event_stream_watcher.username_queue
 
-    def test_acceptingAChallenge(self):
-        pass
+    # def test_challengingEmptyUserDoesNothing(self, event_stream_watcher):
+    #     pass
+    #     event_stream_watcher.challenge_user()
+    #     assert len(self.event_stream_watcher.username_queue) == 0
 
-    def test_sendingABotChallenge(self):
-        pass
+    # def test_acceptingAChallenge(self):
+    #     pass
+
+    # def test_sendingABotChallenge(self):
+    #     pass
