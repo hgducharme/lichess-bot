4b3162fba97cd48da99855bbe3331dc795df8928
Hunter Ducharme <hgducharme@gmail.com>
2023-05-19 15:49:19 -0700

    Tests are passing!

--- test/lichess/test_EventStreamWatcher.py
+++ test/lichess/test_EventStreamWatcher.py
@@ -1,4 +1,7 @@
+import json
+
 from conftest import *
+from lichess.conf import settings
 from lichess.ChessGameManager import ChessGameManager
 from lichess.LichessAPI import LichessAPI
 from lichess.MockChessGameFactory import MockChessGameFactory
@@ -28,7 +31,7 @@ def event_stream_watcher(request, lichess_api, chess_game_manager, mocked_respon
 
     # This turns fake_event_stream into a byte-like iterable object which 
     # is required to mock an API "stream" from the requests library
-    fake_event_stream = str.encode(fake_event)
+    fake_event_stream = str.encode(json.dumps(fake_event))
 
     # EventManager queries the API for the lichess profile information upon instantiation,
     # so we tell responses to expect this call and mock it
@@ -60,8 +63,69 @@ class TestEventStreamWatcher:
 
     @pytest.mark.set_fake_event(fake_gameFinish)
     def test_gameFinishEventDeletesGame(self, event_stream_watcher, chess_game_manager):
-        assert chess_game_manager.number_of_games() == 1
+        chess_game_manager.start_new_game(fake_gameStart)
+        number_of_games = chess_game_manager.number_of_games()
 
         event_stream_watcher.work()
 
-        assert chess_game_manager.number_of_games() == 0
+        assert chess_game_manager.number_of_games() == number_of_games - 1
+
+    @pytest.mark.set_fake_event(fake_outgoingChallenge)
+    def test_outgoingChallengeEventCreatedByTheBotDoesNothing(self, event_stream_watcher, chess_game_manager):
+        number_of_games = chess_game_manager.number_of_games()
+
+        event_stream_watcher.work()
+
+        assert chess_game_manager.number_of_games() == number_of_games
+
+    @pytest.mark.set_fake_event(fake_outgoingChallengeDeclined)
+    def test_challengeDeclinedEventDoesNothing(self, event_stream_watcher, chess_game_manager):
+        number_of_games = chess_game_manager.number_of_games()
+
+        event_stream_watcher.work()
+
+        assert chess_game_manager.number_of_games() == number_of_games
+
+    @pytest.mark.set_fake_event(fake_incomingChallengeCancelled)
+    def test_incomingChallengeCanceledEventDoesNothing(self, event_stream_watcher, chess_game_manager):
+        number_of_games = chess_game_manager.number_of_games()
+
+        event_stream_watcher.work()
+
+        assert chess_game_manager.number_of_games() == number_of_games
+
+    @pytest.mark.set_fake_event(fake_incomingChallenge)
+    def test_incomingChallengeGetsAcceptedIfAcceptingChallengesFlagIsTrue(self, mocked_responses, event_stream_watcher, empty_json_response):
+        settings.ACCEPTING_CHALLENGES = True
+
+        fake_challenge_id = fake_incomingChallenge["challenge"]["id"]
+        url = LichessAPI.construct_url(LichessAPI.URL_ENDPOINTS["accept_challenge"], challengeId = fake_challenge_id)
+        mocked_responses.add(
+            responses.POST,
+            url = url,
+            json = empty_json_response,
+            status = 200,
+        )
+
+        event_stream_watcher.work()
+
+        assert mocked_responses.assert_call_count(url, 1) is True
+
+
+    @pytest.mark.set_fake_event(fake_incomingChallenge)
+    def test_incomingChallengeGetsDeclinedIfAcceptingChallengesFlagIsFalse(self, mocked_responses, event_stream_watcher, empty_json_response):
+        settings.ACCEPTING_CHALLENGES = False
+
+        fake_challenge_id = fake_incomingChallenge["challenge"]["id"]
+        url = LichessAPI.construct_url(LichessAPI.URL_ENDPOINTS["decline_challenge"], challengeId = fake_challenge_id)
+        mocked_responses.add(
+            responses.POST,
+            url = url,
+            json = empty_json_response,
+            status = 200,
+        )
+
+        event_stream_watcher.work()
+
+        # TODO: Verify the response body is included in the API request by the client code
+        assert mocked_responses.assert_call_count(url, 1) is True
