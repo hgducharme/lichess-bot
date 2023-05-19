2eb13a447bfa9e20cad6f987d4395c42acfb199c
Hunter Ducharme <hgducharme@gmail.com>
2023-05-19 15:49:01 -0700

    Added MockChessGame

--- src/lichess/MockChessGameFactory.py
+++ src/lichess/MockChessGameFactory.py
@@ -1,8 +1,22 @@
 from lichess.IChessGameFactory import IChessGameFactory
 
+class MockChessGame:
+    def __init__(self, game_info):
+        self.game_info = game_info
+        pass
+
+    def start(self):
+        pass
+
+    def stop(self):
+        pass
+
+    def join(self):
+        pass
+
 class MockChessGameFactory(IChessGameFactory):
     def __init__(self):
         pass
 
     def create_game(self, game_info):
-        return {}
+        return MockChessGame(game_info)
