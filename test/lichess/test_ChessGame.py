63e212273469d945dc6783c64c63a66bde798272
Hunter Ducharme <hgducharme@gmail.com>
2023-05-21 00:45:40 -0700

    skeleton

---
+++ test/lichess/test_ChessGame.py
@@ -0,0 +1,12 @@
+from conftest import *
+
+class TestChessGame:
+
+    def test_gameState_isSetProperlyOnGameFullEvent(self, chess_game_manager):
+        assert False
+
+    def test_gameState_isSetProperlyOnGameStateEvent(self, chess_game_manager):
+        assert False
+
+    def test_bestMoveIsSentToLichess(self, chess_game_manager):
+        assert False
