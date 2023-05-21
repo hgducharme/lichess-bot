8c9e41e73b3fd81c1569d01e11e80103771aecb0
Hunter Ducharme <hgducharme@gmail.com>
2023-05-20 20:03:40 -0700

    93% test coverage on GameManager

--- src/lichess/MockChessGameFactory.py
+++ src/lichess/MockChessGameFactory.py
@@ -3,31 +3,23 @@ from lichess.IChessGameFactory import IChessGameFactory
 class MockChessGame:
     def __init__(self, game_info):
         self.game_info = game_info
-        pass
+        self.start_counter = 0
+        self.stop_counter = 0
+        self.join_counter = 0
 
     def start(self):
-        pass
+        self.start_counter += 1
 
     def stop(self):
-        pass
+        self.stop_counter += 1
 
     def join(self):
-        pass
+        self.join_counter +=1
 
 class MockChessGameFactory(IChessGameFactory):
     def __init__(self):
-        self._create_game_counter = 0
+        self.create_game_counter = 0
 
     def create_game(self, game_info):
         self.create_game_counter += 1
         return MockChessGame(game_info)
-    
-    @property
-    def create_game_counter(self):
-        return self._create_game_counter
-    
-    @create_game_counter.setter
-    def create_game_counter(self, value):
-        if ( (not isinstance(value, int)) or (value < 0)):
-            raise ValueError("create_game_counter must be an integer greater than or equal to zero")
-        self._create_game_counter = value
--- test/lichess/test_ChessGameManager.py
+++ test/lichess/test_ChessGameManager.py
@@ -5,15 +5,17 @@ class TestChessGameManager:
     def test_startNewGame_addsGameToCollection(self, chess_game_manager):
         assert chess_game_manager.number_of_games() == 0
 
-        chess_game_manager.start_new_game(fake_gameStart)
+        game_started = chess_game_manager.start_new_game(fake_gameStart)
 
+        assert game_started == True
         assert chess_game_manager.number_of_games() == 1
 
     def test_doGamesExist_returnsTrueIfGamesExist(self, chess_game_manager):
         assert chess_game_manager.do_games_exist() == False
 
-        chess_game_manager.start_new_game(fake_gameStart)
+        game_started = chess_game_manager.start_new_game(fake_gameStart)
 
+        assert game_started == True
         assert chess_game_manager.do_games_exist() == True
 
     def test_doGamesExist_returnsFalseIfNoGamesExist(self, chess_game_manager):
@@ -24,12 +26,76 @@ class TestChessGameManager:
     def test_numberOfGames_returnsCorrectAnswer(self, chess_game_manager, n, expected):
         for i in range(0, n):
             dummy_unique_game_info = {"game": {"fullId": i}}
-            chess_game_manager.start_new_game(dummy_unique_game_info)
+            game_started = chess_game_manager.start_new_game(dummy_unique_game_info)
+            assert game_started == True
         
         assert chess_game_manager.number_of_games() == expected
 
-    def test_startNewGame_callsChessGameFactoryOnceIfGameWasAlreadyAdded(self, chess_game_manager, mock_chess_game_factory):
-        chess_game_manager.start_new_game(fake_gameStart)
+    def test_startNewGame_startsGameOnlyOnceIfGameWasAlreadyAdded(self, chess_game_manager, mock_chess_game_factory):
+        game_started1 = chess_game_manager.start_new_game(fake_gameStart)
+        game_started2 = chess_game_manager.start_new_game(fake_gameStart)        
+
+        assert game_started1 == True
+        assert game_started2 == False
+        assert mock_chess_game_factory.create_game_counter == 1
+
+    def test_startNewGame_rejectsGameIfNotAcceptingGamesIsTrue(self, chess_game_manager, mock_chess_game_factory):
+        chess_game_manager.is_accepting_games = False
+
+        game_started = chess_game_manager.start_new_game(fake_gameStart)
+
+        assert game_started == False
+        assert mock_chess_game_factory.create_game_counter == 0
+
+    def test_terminateAllGames_deletesAllGames(self, chess_game_manager):
+        import copy
+
+        assert chess_game_manager.number_of_games() == 0
+
+        game_id1 = fake_gameStart["game"]["fullId"]
+
+        # Create a second game
+        game_id2 = game_id1 + "2"
+        fake_game2 = copy.deepcopy(fake_gameStart)
+        fake_game2["game"]["fullId"]  = game_id2
+
+        game_started1 = chess_game_manager.start_new_game(fake_gameStart)
+        game_started2 = chess_game_manager.start_new_game(fake_game2)
+
+        assert game_started1 == True
+        assert game_started2 == True
+
+        games = chess_game_manager.games
+
+        chess_game_manager.terminate_all_games()
+
+        assert chess_game_manager.number_of_games() == 0
+        for game in games:
+            assert game.stop_counter == 1
+            assert game.join_counter == 1
+
+    def test_terminateGame_stopsAndJoinsToThreadAndDeletesGameFromRepository(self, chess_game_manager):
+        assert chess_game_manager.number_of_games() == 0
+
+        game_id = fake_gameStart["game"]["fullId"]
         chess_game_manager.start_new_game(fake_gameStart)
 
-        assert mock_chess_game_factory.create_game_counter == 1
+        game = chess_game_manager.games[game_id]
+
+        chess_game_manager.terminate_game(game_id)
+
+        assert game.stop_counter == 1
+        assert game.join_counter == 1
+        assert chess_game_manager.number_of_games() == 0
+
+    def test_terminateGame_doesNotThrowExceptionIfGameIsNotFound(self, chess_game_manager):
+        game_id = fake_gameStart["game"]["fullId"]
+        try:
+            chess_game_manager.terminate_game(game_id)
+        except KeyError as err:
+            assert False, f"fake_gameStart with ID {game_id} raised a KeyError exception."
+
+    @pytest.mark.parametrize("value", [1, "value", [], {}, ])
+    def test_isAcceptingGamesProperty_onlyAllowsBooleanValues(self, chess_game_manager, value):
+        with pytest.raises(TypeError):
+            chess_game_manager.is_accepting_games = value
